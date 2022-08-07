fd6f8bd5daa006504dd5e051bf0cd6b681d00003
guandjoy <guandjoy@gmail.com>
2022-08-07 14:45:23 +0000

    Remove submodules

---
+++ nvim/init.vim
@@ -0,0 +1,143 @@
+call plug#begin('~/AppData/Local/nvim/plugged')
+Plug 'neovim/nvim-lspconfig'
+
+Plug 'ryanoasis/vim-devicons'
+Plug 'github/copilot.vim'
+" Git
+Plug 'tpope/vim-fugitive'
+" Statusline/tablines
+Plug 'vim-airline/vim-airline'
+Plug 'zefei/vim-wintabs'
+" Highlight syntax
+" Plug 'sheerun/vim-polyglot'
+" Themes
+Plug 'rakr/vim-one'
+
+" Yank to system clipboard
+Plug 'jabirali/vim-tmux-yank'
+
+" Commenting
+Plug 'tpope/vim-commentary' " Support JSX
+
+" lf file manager support
+Plug 'ptzz/lf.vim'
+Plug 'voldikss/vim-floaterm'
+
+" fzf
+Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+Plug 'junegunn/fzf.vim'
+
+" telescope
+Plug 'nvim-lua/plenary.nvim'
+Plug 'nvim-telescope/telescope.nvim'
+
+" terminal
+Plug 'voldikss/vim-floaterm'
+
+" tmux integration
+Plug 'christoomey/vim-tmux-navigator'
+
+" Code formatter and linter
+Plug 'jose-elias-alvarez/null-ls.nvim'
+
+" Autocompletion
+Plug 'neovim/nvim-lspconfig'
+Plug 'hrsh7th/cmp-nvim-lsp'
+Plug 'hrsh7th/cmp-nvim-lua'
+Plug 'hrsh7th/cmp-buffer'
+Plug 'hrsh7th/cmp-path'
+Plug 'hrsh7th/cmp-cmdline'
+Plug 'hrsh7th/nvim-cmp'
+" For vsnip users.
+Plug 'hrsh7th/cmp-vsnip'
+Plug 'hrsh7th/vim-vsnip'
+
+call plug#end()
+
+" nvim-cmp
+set completeopt=menu,menuone,noselect
+
+set number
+
+" General settings
+set tabstop=4 
+
+" Appearance
+set termguicolors
+colorscheme one
+set background=dark
+let g:airline_theme='one'
+
+" Prettier
+command! -nargs=0 Prettier :CocCommand prettier.formatFile
+
+let g:coc_global_extensions = [
+  \ 'coc-tailwindcss',
+  \ 'coc-tsserver',
+  \ 'coc-toml',
+  \ 'coc-svg',
+  \ 'coc-prettier', 
+  \ 'coc-pairs',
+  \ 'coc-json', 
+  \ 'coc-eslint',
+  \ 'coc-css',
+  \]
+
+" Maps
+let mapleader = "\<Space>"
+inoremap jj <Esc>
+nmap <S-Enter> O<Esc>
+nmap <CR> o<Esc>
+nnoremap <esc> :noh<return><esc> 
+
+" ESLint
+nmap <S-f> <Plug>(coc-codeaction)
+" Rename as VSCode
+nmap <F2> <Plug>(coc-rename)
+" Commenting
+xmap <C-/> <Plug>Commentary
+nmap <C-/> <Plug>CommentaryLine
+" Rename as VSCode
+nmap <leader>rn <Plug>(coc-rename)
+" Move lines up/down. Alternative to alt+up/down from VSCode
+nnoremap <C-j> :m .+1<CR>==
+nnoremap <C-k> :m .-2<CR>==
+inoremap <C-j> <Esc>:m .+1<CR>==gi
+inoremap <C-k> <Esc>:m .-2<CR>==gi
+vnoremap <C-j> :m '>+1<CR>gv=gv
+vnoremap <C-k> :m '<-2<CR>gv=gv
+" Wintab navigate
+map <S-h> <Plug>(wintabs_previous)
+map <S-l> <Plug>(wintabs_next)
+map <S-w> <Plug>(wintabs_close)
+" Floaterm
+nnoremap   <silent>   <leader>j   :FloatermToggle<CR>
+tnoremap   <silent>   <leader>j   <C-\><C-n>:FloatermToggle<CR>
+" FZF
+nmap <C-p> :GFiles<CR>
+nmap <leader>s :Rg<CR>
+" Telescope
+" Using Lua functions
+nnoremap <leader>ff <cmd>lua require('telescope.builtin').find_files()<cr>
+nnoremap <leader>fg <cmd>lua require('telescope.builtin').live_grep()<cr>
+nnoremap <leader>fb <cmd>lua require('telescope.builtin').buffers()<cr>
+nnoremap <leader>fh <cmd>lua require('telescope.builtin').help_tags()<cr>
+
+"LF
+let g:lf_map_keys = 0
+map <leader>k :Lf<CR>
+
+""""""""""""""""
+" HIGHLIGHTING "
+""""""""""""""""
+
+" highlight link CocHighlightText Visual
+highlight CursorColumn ctermbg=16 gui=underline guisp=#454C5E
+highlight CocWarningHighlight ctermfg=Yellow gui=undercurl guisp=Yellow
+highlight link CocInfoHighlight JavaScriptBraces
+highlight link CocErrorHighlight Error
+highlight CocHintHighlight ctermfg=Red  guifg=Red
+" Tabline
+highlight TablineFill ctermfg=59 ctermbg=17 guifg=#748CB7 guibg=#3e4452
+
+lua require("basic")
---
+++ nvim/lua/basic.lua
@@ -0,0 +1,146 @@
+-- Formatter
+local lsp_formatting = function(bufnr)
+	vim.lsp.buf.format({
+		filter = function(client)
+			-- apply whatever logic you want (in this example, we'll only use null-ls)
+			return client.name == "null-ls"
+		end,
+		bufnr = bufnr,
+	})
+end
+-- if you want to set up formatting on save, you can use this as a callback
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+-- Use an on_attach function to only map the following keys
+-- after the language server attaches to the current buffer
+local on_attach = function(client, bufnr)
+	-- Format code on save
+	if client.supports_method("textDocument/formatting") then
+		vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+		vim.api.nvim_create_autocmd("BufWritePre", {
+			group = augroup,
+			buffer = bufnr,
+			callback = function()
+				lsp_formatting(bufnr)
+			end,
+		})
+	end
+
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
+
+	-- Mappings.
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = { noremap = true, silent = true, buffer = bufnr }
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gt", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "<leader>dl", "<cmd>Telescope diagnostics<cr>", bufopts)
+	vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, bufopts)
+end
+
+require("null-ls").setup({
+	on_attach = on_attach,
+	sources = {
+		require("null-ls").builtins.formatting.stylua,
+		require("null-ls").builtins.diagnostics.eslint,
+		require("null-ls").builtins.completion.spell,
+		require("null-ls").builtins.formatting.prettierd,
+		-- tailwind
+		require("null-ls").builtins.formatting.rustywind,
+		-- ruby
+		-- require("null-ls").builtins.formatting.rufo,
+		require("null-ls").builtins.formatting.rubocop,
+		-- require("null-ls").builtins.diagnostics.rubocop,
+	},
+})
+
+-- Setup lspconfig.
+local capabilities = require("cmp_nvim_lsp").update_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+require("lspconfig").gopls.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+require("lspconfig")["tsserver"].setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+require("lspconfig").tailwindcss.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+})
+require("lspconfig").graphql.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+}) -- graphql
+require("lspconfig").eslint.setup({
+	on_attach = on_attach,
+	capabilities = capabilities,
+}) -- eslint
+
+-- Setup nvim-cmp.
+local cmp = require("cmp")
+
+cmp.setup({
+	snippet = {
+		-- REQUIRED - you must specify a snippet engine
+		expand = function(args)
+			vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+			-- require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+			-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+			-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+		end,
+	},
+	experimental = {
+		native_menu = false,
+	},
+	window = {
+		-- completion = cmp.config.window.bordered(),
+		-- documentation = cmp.config.window.bordered(),
+	},
+	mapping = cmp.mapping.preset.insert({
+		["<C-b>"] = cmp.mapping.scroll_docs(-4),
+		["<C-f>"] = cmp.mapping.scroll_docs(4),
+		["<C-Space>"] = cmp.mapping.complete(),
+		["<C-e>"] = cmp.mapping.abort(),
+		["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+	}),
+	sources = cmp.config.sources({
+		{ name = "nvim_lsp" },
+		{ name = "vsnip" }, -- For vsnip users.
+		-- { name = 'luasnip' }, -- For luasnip users.
+		-- { name = 'ultisnips' }, -- For ultisnips users.
+		-- { name = 'snippy' }, -- For snippy users.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Set configuration for specific filetype.
+cmp.setup.filetype("gitcommit", {
+	sources = cmp.config.sources({
+		{ name = "cmp_git" }, -- You can specify the `cmp_git` source if you were installed it.
+	}, {
+		{ name = "buffer" },
+	}),
+})
+
+-- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline("/", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = {
+		{ name = "buffer" },
+	},
+})
+
+-- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
+cmp.setup.cmdline(":", {
+	mapping = cmp.mapping.preset.cmdline(),
+	sources = cmp.config.sources({
+		{ name = "path" },
+	}, {
+		{ name = "cmdline" },
+	}),
+})
